1464e58bfd6abf7d355ca9ba8b4a3f20b2790946
NhatHM4 <haminhnhat711@gmail.com>
2023-04-27 22:06:54 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
-import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data';
+import Starter from './tutorial/03-conditional-rendering/starter/03-hooks-rule';
+import Final from './tutorial/03-conditional-rendering/final/03-hooks-rule';
 function App() {
   return (
     <div className='container'>
-      <Starter/>
-      {/* <Final /> */}
+      {/* <Starter/> */}
+      <Final />
     </div>
   );
 }
