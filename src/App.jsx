f2e008cb5e90616aca696daad80db28859961038
NhatHM4 <haminhnhat711@gmail.com>
2023-09-07 22:21:43 +0700

    chan

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Final from './tutorial/09-context-api/final/context/Navbar';
-import Starter from './tutorial/10-useReducer/starter/TodoListGlobalState';
+import Starter from './tutorial/11-performance/starter/02-lower-state-challenge';
 
 function App() {
   return (
